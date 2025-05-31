9c21b2fe3cf04772049ce9622e630be7f02ff3c9
Sean053047 <sadlamb910803@gmail.com>
2025-05-31 15:01:09 +0800

    Add submodule:DepthAnythingV2

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# 2025 Edge AI Lab4: Raspi Deployment
+
+## Requirement
+python version:3.10
--- eval_model.py
+++ eval_model.py
@@ -1,42 +1,124 @@
-from datasets import load_dataset
 from transformers import AutoImageProcessor, AutoModelForDepthEstimation
 from transformers.modeling_outputs import DepthEstimatorOutput
 from transformers.models.dpt.image_processing_dpt import DPTImageProcessor
 import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import Compose
+from torch.nn import functional as F
+
+import os.path as osp
+import cv2
 import numpy as np
 from PIL import Image
 import requests
+from collections import defaultdict
+from DepthAnythingV2.metric_depth.dataset.transform import Resize, NormalizeImage, PrepareForNet, Crop
+from DepthAnythingV2.metric_depth.util.metric import eval_depth
+from DepthAnythingV2.depth_anything_v2.dpt import DepthAnythingV2
 
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
+from datasets import load_dataset
 
-image_processor = AutoImageProcessor.from_pretrained("depth-anything/Depth-Anything-V2-Small-hf")
-model = AutoModelForDepthEstimation.from_pretrained("depth-anything/Depth-Anything-V2-Small-hf")
+class CustomVIKITTI2(Dataset):
+    def __init__(self, data_root, filelist_path, mode, size=(518, 518)):
+        self.mode = mode
+        self.size = size
+        self.data_root = data_root
+        with open(filelist_path, 'r') as f:
+            self.filelist = f.read().splitlines()
+        
+        net_w, net_h = size
+        self.transform = Compose([
+            Resize(
+                width=net_w,
+                height=net_h,
+                resize_target=True if mode == 'train' else False,
+                keep_aspect_ratio=False,
+                ensure_multiple_of=14,
+                resize_method='lower_bound',
+                image_interpolation_method=cv2.INTER_CUBIC,
+            ),
+            NormalizeImage(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            PrepareForNet(),
+        ] + ([Crop(size[0])] if self.mode == 'train' else []))
+    def __len__(self):
+        return len(self.filelist)
+    def __getitem__(self, idx):
+        img_path = osp.join(self.data_root, self.filelist[idx].split(' ')[0])
+        depth_path = osp.join(self.data_root, self.filelist[idx].split(' ')[1])
+        
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) / 255.0
+        
+        depth = cv2.imread(depth_path, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH) / 100.0  # cm to m
+        
+        sample = self.transform({'image': image, 'depth': depth})
 
-# prepare image for the model
-inputs = image_processor(images=image, return_tensors="pt")
+        sample['image'] = torch.from_numpy(sample['image'])
+        sample['depth'] = torch.from_numpy(sample['depth'])
+        
+        sample['valid_mask'] = (sample['depth'] <= 80)
+        
+        sample['image_path'] = self.filelist[idx].split(' ')[0]
+        return sample
 
-with torch.no_grad():
-    outputs = model(**inputs)
-# interpolate to original size and visualize the prediction
-post_processed_output = image_processor.post_process_depth_estimation(
-    outputs,
-    target_sizes=[(image.height, image.width)],
-)
+def init_model(encoder:str='vitb'):
+    model_configs = {
+        'vits': {'encoder': 'vits', 'features': 64, 'out_channels': [48, 96, 192, 384]},
+        'vitb': {'encoder': 'vitb', 'features': 128, 'out_channels': [96, 192, 384, 768]},
+        'vitl': {'encoder': 'vitl', 'features': 256, 'out_channels': [256, 512, 1024, 1024]},
+        'vitg': {'encoder': 'vitg', 'features': 384, 'out_channels': [1536, 1536, 1536, 1536]}
+    }
+    model = DepthAnythingV2(**model_configs[encoder])
+    model.load_state_dict(torch.load(f'checkpoints/depth_anything_v2_{encoder}.pth', map_location='cpu'))
+    return model
 
-predicted_depth = post_processed_output[0]["predicted_depth"]
-depth = (predicted_depth - predicted_depth.min()) / (predicted_depth.max() - predicted_depth.min())
-depth = depth.detach().cpu().numpy() * 255
-depth = Image.fromarray(depth.astype("uint8"))
-depth.save("depth_anything_output.png")
+def init_huggingface_model(pretrained:str='depth-anything/Depth-Anything-V2-Small-hf'):
+    image_processor = AutoImageProcessor.from_pretrained(pretrained)
+    model = AutoModelForDepthEstimation.from_pretrained(pretrained)
+    return image_processor, model
 
 
+@ torch.no_grad()
+def evaluate(args):
+    batch_size = 1
+    filelist_path = 'vkitti2_train.txt'
+    device = f'cuda:{args.gpu}' if torch.cuda.is_available() else 'cpu'
+    dataset = CustomVIKITTI2(data_root=args.vkitti2_root, filelist_path=filelist_path, mode='test', size=(518, 518))
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+    depth_metrics = defaultdict(list)    
+    
+    model = init_model(encoder=args.encoder).to(device)
+    for batch in dataloader:
+        image = batch['image'].to(device)
+        depth = batch['depth'].to(device)
+        valid_mask = batch['valid_mask'].to(device)
+        height, width = depth.shape[1], depth.shape[2]
+        
+        pred_depth = model(image)
+        pred_depth = F.interpolate(pred_depth.unsqueeze(1), size=(height, width), mode='bicubic', align_corners=False).squeeze(1)
+        
+        pred_depth = pred_depth[valid_mask] 
+        depth = depth [valid_mask]
+        print(pred_depth.shape, depth.shape)
+        print(pred_depth.shape, depth.shape)
+        metric = eval_depth(pred_depth, depth)
+        print(metric)
+        exit()
+            
+    
+
+official_model = "depth-anything/Depth-Anything-V2-Small-hf"
 
 def parse_args():
     import argparse
     parser = argparse.ArgumentParser(description="Evaluate Depth Estimation Model")
-    parser.add_argument("--model", type=str, default="depth-anything/Depth-Anything-V2-Small-hf", help="Model name or path")
+    parser.add_argument("--pretrained", type=str, default="depth-anything/Depth-Anything-V2-Small-hf", help="Model name or path")
+    parser.add_argument("--vkitti2_root", type=str, default="/data//vkitti2", help="Root directory for VKITTI2 dataset")
+    parser.add_argument("--encoder", type=str, default="vitb", choices=["vits", "vitb", "vitl"], help="Encoder type for DepthAnythingV2")
+    parser.add_argument("--gpu", type=int, default=0, help="GPU ID to use for evaluation")
     return parser.parse_args()
 
 if __name__ == "__main__":
-    ...
+    args = parse_args()
+    evaluate(args)
+    print("Depth estimation completed and saved as 'depth_anything_output.png'.")
